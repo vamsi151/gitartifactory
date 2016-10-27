9ca930706f2af67b8aef73a4b17872123e780c91
vamsi151 <vamsikrishna6855@gmail.com>
2016-10-27 14:14:52 +0530

    latest modified code

--- gittoartifactory/artifactoryfilepush.py
+++ gittoartifactory/artifactoryfilepush.py
@@ -1,52 +1,53 @@
 import requests
 import hashlib
-import shutil
-import os
-import re
 import getpass
 
+def pushfiletoartifactory(targetLocation , *args, **kwargs):
+
+    try:
+        pre_check(targetLocation)
+
+        # Intializing Variables required
+
+        artifactory_url = raw_input("User Please Enter Artifactory URL : ")
+
+        #artifactory_url = "http://153.64.146.90:8080/artifactory/OSAT-STABLE/Test2/svn2git"       # your artifactory instance
+        
+        filelocation = raw_input("User Please Enter file location Example: /root/filename (or) D:/filenmae :")
+
+        artifactory_username = raw_input("User Please Quicklook Id : ")
+
+        #artifactory_username = 'vs186041'         # Quicklook Id :
+
+        artifcatory_password = getpass.getpass('Password:')
 
 
-def pushfiletoartifactory(*args, **kwargs):
+        filename = filelocation.split('/')[-1]
+        
+        upload_url = artifactory_url + '/' + filename
+        
+        content_type = 'application/archive'
+        
+        # checksums are useful for making sure the upload was successful
 
+        md5_checksum = hashlib.sha1(open(value).read()).hexdigest()
 
-    artifactory_url = raw_input("User Please Enter Artifactory URL : ")
+        headers = {'content-type': content_type,
+                   'X-Checksum-Md5': hashlib.md5(open(value).read()).hexdigest(),
+                   'X-Checksum-Sha1': md5_checksum}
 
-    #artifactory_url = "http://**************/artifactory/OSAT-STABLE/Test2/svn2git"       # your artifactory instance
+        # pushing file data to Artifactory
 
+        with open(value, 'rb') as f:
+            response = requests.put(upload_url ,auth=(artifactory_username , artifcatory_password ),data=f, headers=headers)
 
-    filelocation = raw_input("User Please Enter file location which need to be pushed to Artifactory :")
+        # check for success
 
-    artifactory_username = raw_input("User Please enter Quicklook Id : ")
-
-    #artifactory_username = 'vs186041'         # your username
-
-    artifcatory_password = getpass.getpass('Password:')
-
-    filename = filelocation.split('/')[-1]
-    upload_url = artifactory_url + '/' + filename
-    content_type = 'application/archive'
-
-    # checksums are useful for making sure the upload was successful
-
-    md5_checksum = hashlib.sha1(open(filelocation).read()).hexdigest()
-
-    headers = {'content-type': content_type,
-               'X-Checksum-Md5': hashlib.md5(open(filelocation).read()).hexdigest(),
-               'X-Checksum-Sha1': md5_checksum}
-
-    
-    print ("File transferring to location {} is in Progress......".format(upload_url))
-    # pushing file data to Artifactory
-
-    with open(filelocation, 'rb') as f:
-        response = requests.put(upload_url ,auth=(artifactory_username , artifcatory_password ),data=f, headers=headers)
-
-    # check for success
-    if response.status_code != 201:
-        raise Exception("something went wrong!!!!")
-    else:
-        DownloadURL =  response.json()['downloadUri']  # download url for this new artifact
-        print   "Succesfully done you can download repo from Location : {} ".format(DownloadURL)
-
+        if response.status_code != 201:
+            raise Exception("something went wrong!!!!")
+        else:
+            DownloadURL =  response.json()['downloadUri']  # download url for this new artifact
+            print   "Succesfully done you can download repo from Location : {} ".format(DownloadURL)
+    except Exception,e:
+        print "Exception raised : ", e
 
